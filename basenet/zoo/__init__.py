a28567260cd5e380865e81c6fcc68f420a381797
lujie <relu@lujies-Mac-mini.local>
2019-11-20 20:19:20 +0800

    move basenet out the project

--- basenet/zoo/__init__.py
+++
@@ -1,7 +0,0 @@
-from .lenet      import LeNets
-from .alexnet    import AlexNet
-from .vgg        import VGG
-from .inception  import GoogLeNet
-from .resnet     import ResNet
-from .densenet   import DenseNet
-from .squeezenet import SqueezeNet
